bf2a899118d56ed56916bb8e6fa357709e686afb
GK1100 <gaurav30.kumavat@gmail.com>
2026-05-01 16:46:13 +0530

    fixed

--- api/app.py
+++ api/app.py
@@ -3,4 +3,5 @@ import os
 
 # Add src to path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
-from src.orchestrator import app
+# from src.orchestrator import app
+from orchestrator import app
