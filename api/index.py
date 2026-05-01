a195067c696991d9c8fb5e4b79338d6b61537d78
GK1100 <gaurav30.kumavat@gmail.com>
2026-05-01 17:01:16 +0530

    fixeddd

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from orchestrator import app
+from src.orchestrator import app
